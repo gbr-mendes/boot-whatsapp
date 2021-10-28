2b967408ce10cd19323b8eb7a23e71ad93c24651
gbr-mendes <59442476+gbr-mendes@users.noreply.github.com>
2021-10-27 22:41:13 -0300

    Remove class file

--- classes.py
+++
@@ -1,8 +0,0 @@
-class Contact:
-    def __init__(self, nicho,empresa,telefone,estado,municipio):
-        self.nicho = nicho
-        self.empresa = empresa
-        self.telefone = telefone
-        self.estado = estado
-        self.municipio = municipio
-        
